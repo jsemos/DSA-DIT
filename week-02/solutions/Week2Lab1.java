5d8fab5fcd0a5f0aa120ed72783ca8810cdb2bba
JJ <jsemos05@gmail.com>
2025-05-06 09:49:39 +1000

    Add files via upload

---
+++ week-02/solutions/Week2Lab1.java
@@ -0,0 +1,13 @@
+/* DSA Week 2 Lab 1 */
+
+public class Week2Lab1 {
+
+   public static void main(String []args) {
+      System.out.println("Jerome"); // prints my name
+      System.out.println('M'); //prints if your Male or Female
+      System.out.println(2025); //prints a number
+      System.out.println(2.50); //prints a decimal number
+      System.out.println(2*5); //prints multiplication
+      System.out.println(2-5); //prints addition
+   }
+}
---
+++ week-02/solutions/Week2Lab2.java
@@ -0,0 +1,29 @@
+/* DSA Week 2 Lab 2 */
+
+public class Week2Lab2 {
+
+   public static void main(String []args) {
+      
+      //create local variable names and align values to each
+      String name = "Jerome";
+      char gender ='M'; 
+      int num = 2025;
+      int num2 = (2*5);
+      
+      int num3;
+      num3 = (2+5);
+      
+      int x = 5; 
+      int y = 2;
+      int num4 = (x - y);
+     
+      
+      
+      System.out.println(name); // prints my name
+      System.out.println(gender); //prints if your Male or Female
+      System.out.println(num); //prints a number
+      System.out.println(num2); //prints multiplication
+      System.out.println(num3); //prints aditional
+      System.out.println(num4); //prints subtraction
+   }
+}
